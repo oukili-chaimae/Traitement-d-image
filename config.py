17e2eeb87187ffa875a9ee647014446847009f46
oukili-chaimae <chaimaeou06@gmail.com>
2021-12-21 16:46:08 +0100

    config,run

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import projet
 
 
+from flask import Flask, app
+
 SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
 
 PROJET_ID = 1200420960103822
--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import projet
 from projet import app
 
+from flask_mysqldb import MySQL
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "projet"
+
+mysql = MySQL(app)
+
 if __name__ == "__main__":
         app.run(debug=True)
