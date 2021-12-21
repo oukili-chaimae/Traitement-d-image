6e2d814bbf6b662feb4cae8584b2c8f5c8745a84
oukili-chaimae <chaimaeou06@gmail.com>
2021-12-21 20:34:28 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 import projet
 from projet import app
 
-from flask_mysqldb import MySQL
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "projet"
-
-mysql = MySQL(app)
 
 if __name__ == "__main__":
         app.run(debug=True)
